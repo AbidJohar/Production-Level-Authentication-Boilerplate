c04c302be2639cad843d162fb235cae3a2611cc9
Javascript_Developer <ah1502651@gmail.com>
2026-02-16 15:10:25 +0500

    Fix the issues

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
-import { toast } from "sonner"; // 1. Import from sonner
+import { toast } from "sonner";  
 
 const Login = () => {
   const [state, setState] = useState("Sign Up");
